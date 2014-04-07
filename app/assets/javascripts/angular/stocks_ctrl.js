26ed02171709baff0d6265b5c24af71ff46bc180
Feather <lisafeather@gmail.com>
2014-04-07 16:47:07 -0700

    angular loaded properly and responding with seed data

--- app/assets/javascripts/angular/stocks_ctrl.js
+++ app/assets/javascripts/angular/stocks_ctrl.js
@@ -1,7 +1,3 @@
-var app = angular.module("StockWatch", ['ngResource']);
-
-console.log("in main");
-
 app.controller('StocksCtrl', ['$scope', '$resource', function($scope, $resource) {
   console.log("in stocks controller");
   var Stocks = $resource('/api/stocks');
